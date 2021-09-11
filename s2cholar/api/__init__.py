028a8ae0d9812ae97b0ad82a95d09040b14a8e3f
Luiz Otavio Vilas Boas Oliveira <luiz.vbo@gmail.com>
2021-09-11 21:20:11 +0200

    ENH: Add code generated from the swagger

---
+++ s2cholar/api/__init__.py
@@ -0,0 +1,7 @@
+from __future__ import absolute_import
+
+# flake8: noqa
+
+# import apis into api package
+from s2cholar.api.author_api import AuthorApi
+from s2cholar.api.paper_api import PaperApi
