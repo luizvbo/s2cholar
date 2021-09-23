1b238545a37aa620f2c71d177bb942b54151b598
Luiz Otavio Vilas Boas Oliveira <luiz.vbo@gmail.com>
2021-09-23 17:11:15 +0200

    TST: Add simple tests for get_paper_authors

--- test/test_paper_api.py
+++ test/test_paper_api.py
@@ -10,10 +10,6 @@
 
 from __future__ import absolute_import
 
-import s2cholar
-from s2cholar.api.paper_api import PaperApi
-from s2cholar.rest import ApiException
-
 
 def test_get_paper(paper_api):
     """Test case for get_paper
