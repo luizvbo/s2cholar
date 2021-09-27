e4495611930f1ac3f7dec6e58e093c7571bde3d8
Luiz Otavio Vilas Boas Oliveira <luiz.vbo@gmail.com>
2021-09-27 22:38:09 +0200

    BUG: Fix fixture error

--- test/test_author_api.py
+++ test/test_author_api.py
@@ -4,26 +4,16 @@
     Literature Graph Service
 
     Fetch paper and author data from the Semantic Scholar corpus
+
 """
-
-
-from __future__ import absolute_import
-
-import s2cholar
-from s2cholar.api.author_api import AuthorApi
-from s2cholar.rest import ApiException
-
-
-"""AuthorApi unit test stubs"""
-
-def test_get_graph_get_author(self):
+def test_get_graph_get_author():
     """Test case for get_graph_get_author
 
     Details about an author
     """
     pass
 
-def test_get_graph_get_author_papers(self):
+def test_get_graph_get_author_papers():
     """Test case for get_graph_get_author_papers
 
     Details about an author's papers
