c59d3aa7febec6d19a88d2dde840786cdacc4bb4
Luiz Otavio Vilas Boas Oliveira <luiz.vbo@gmail.com>
2021-09-28 23:52:32 +0200

    TST: Add test for get_author

--- test/test_author_api.py
+++ test/test_author_api.py
@@ -6,12 +6,22 @@
     Fetch paper and author data from the Semantic Scholar corpus
 
 """
-def test_get_graph_get_author():
+AUTHOR_ID = '145872832'
+
+def test_get_author(author_api):
     """Test case for get_graph_get_author
 
     Details about an author
     """
-    pass
+    expected_fields = [
+        'author_id', 'external_ids', 'url', 'name', 'aliases', 'affiliations',
+        'homepage', 'papers'
+    ]
+    author = author_api.get_author(AUTHOR_ID)
+    assert author[1] == 200
+    for field in expected_fields:
+        assert hasattr(author[0], field)
+
 
 def test_get_graph_get_author_papers():
     """Test case for get_graph_get_author_papers
