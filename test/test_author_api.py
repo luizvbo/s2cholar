e87ce97a09ddf2c7c31c83805ddd7aba36a3ce10
Luiz Otavio Vilas Boas Oliveira <luiz.vbo@gmail.com>
2021-09-29 08:42:25 +0200

    TST: Add tests for get_author_papers

--- test/test_author_api.py
+++ test/test_author_api.py
@@ -23,9 +23,20 @@ def test_get_author(author_api):
         assert hasattr(author[0], field)
 
 
-def test_get_graph_get_author_papers():
+def test_get_author_papers(author_api):
     """Test case for get_graph_get_author_papers
 
     Details about an author's papers
     """
-    pass
+    expected_fields = ['data', 'next', 'offset']
+    expected_data_fields = [
+        'paperId', 'externalIds', 'url', 'title', 'abstract', 'venue', 'year',
+        'referenceCount', 'citationCount', 'influentialCitationCount',
+        'isOpenAccess', 'fieldsOfStudy', 'authors', 'citations', 'references'
+    ]
+    author = author_api.get_author_papers(AUTHOR_ID)
+    assert author[1] == 200
+    for field in expected_fields:
+        assert hasattr(author[0], field)
+    for field in expected_data_fields:
+        assert field in author[0].data[0]
